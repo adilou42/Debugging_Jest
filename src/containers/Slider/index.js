f9365e499b25305b9d5d3b75a716fc3b127fae87
Adil Yakdi <yakdi.adil@gmail.com>
2024-08-17 12:29:27 +0200

    ✅ project done

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -7,64 +7,64 @@ import "./style.scss";
 const SLIDE_INTERVAL = 5000;
 
 const Slider = () => {
-  const { data } = useData();
-  const [index, setIndex] = useState(0); // starts with the first event
-  const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
-);
+    const { data } = useData();
+    const [index, setIndex] = useState(0); // starts with the first event
+    const byDateDesc = data?.focus.sort((evtA, evtB) =>
+        new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
+    );
 
-  const nextCard = () => {
-    setIndex((prevIndex) => (prevIndex < 2 ? prevIndex + 1 : 0));
-  };
+    const nextCard = () => {
+        setIndex((prevIndex) => (prevIndex < 2 ? prevIndex + 1 : 0));
+    };
 
-  useEffect(() => {
-    const timer = setTimeout(nextCard, SLIDE_INTERVAL); 
-    return () => clearTimeout(timer); // Cleanup the timeout on unmount
-  }, [index]); 
+    useEffect(() => {
+        const timer = setTimeout(nextCard, SLIDE_INTERVAL);
+        return () => clearTimeout(timer); // Cleanup the timeout on unmount
+    }, [index]);
 
+    function changeSlide(radioIdx) {
+        setIndex(radioIdx);
+    }
 
-  function changeSlide(radioIdx) {
-    setIndex(radioIdx);
-  }
-  
-  return (
-    <div className="SlideCardList">
-      {byDateDesc?.map((event, idx) => (
-        <>
-          <div
-            key={event.title}
-            className={`SlideCard SlideCard--${
-              index === idx ? "display" : "hide"
-            }`}
-          >
-            <img src={event.cover} alt="forum" />
-            <div className="SlideCard__descriptionContainer">
-              <div className="SlideCard__description">
-                <h3>{event.title}</h3>
-                <p>{event.description}</p>
-                <div>{getMonth(new Date(event.date))}</div>
-              </div>
+    return (
+        <div className="SliderSection">
+            <div className="SlideCardList">
+                {byDateDesc?.map((event, idx) => (
+                    <div key={`eventFocus-${event.id}`}>
+                        <div
+                            className={`SlideCard SlideCard--${
+                                index === idx ? "display" : "hide"
+                            }`}
+                        >
+                            <img src={event.cover} alt="forum" />
+                            <div className="SlideCard__descriptionContainer">
+                                <div className="SlideCard__description">
+                                    <h3>{event.title}</h3>
+                                    <p>{event.description}</p>
+                                    <div>{getMonth(new Date(event.date))}</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                ))}
             </div>
-          </div>
-          <div className="SlideCard__paginationContainer">
-            <div className="SlideCard__pagination">
-              {byDateDesc.map((_, radioIdx) => (
-                <div className="bulletPoint">
-                  <input
-                    onClick={() => changeSlide(radioIdx)}
-                    key={`${event.cover}-${index}`}
-                    type="radio"
-                    name="radio-button"
-                    defaultChecked={index === radioIdx} // use index to select the current event
-                  />
-                </div>
-              ))}
+            <div className="SlideCard__paginationBullet">
+                {byDateDesc?.map((event, radioIdx) => (
+                    <div
+                        className="bulletPoint"
+                        key={`BulletPoint-${event.id}`}
+                    >
+                        <input
+                            onClick={() => changeSlide(radioIdx)}
+                            type="radio"
+                            name="radio-button"
+                            defaultChecked={index === radioIdx} // use index to select the current event
+                        />
+                    </div>
+                ))}
             </div>
-          </div>
-        </>
-      ))}
-    </div>
-  );
+        </div>
+    );
 };
 
 export default Slider;
